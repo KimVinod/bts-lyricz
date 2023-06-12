3ed719f0775422167c193884f0339495d84ac2b3
Kim Vinod <79586514+KimVinod@users.noreply.github.com>
2023-06-12 21:35:38 +0530

    Update ui_constants.dart

--- lib/utils/ui_constants.dart
+++ lib/utils/ui_constants.dart
@@ -1,3 +1,15 @@
+import 'package:bts_lyrics_app/screens/discography/albums_jp.dart';
+import 'package:bts_lyrics_app/screens/discography/albums_kr.dart';
+import 'package:bts_lyrics_app/screens/discography/albums_uo.dart';
+import 'package:bts_lyrics_app/screens/discography/digital_singles.dart';
+import 'package:bts_lyrics_app/screens/discography/songs_uo.dart';
+import 'package:bts_lyrics_app/screens/members/hoseok.dart';
+import 'package:bts_lyrics_app/screens/members/jimin.dart';
+import 'package:bts_lyrics_app/screens/members/jin.dart';
+import 'package:bts_lyrics_app/screens/members/jungkook.dart';
+import 'package:bts_lyrics_app/screens/members/namjoon.dart';
+import 'package:bts_lyrics_app/screens/members/taehyung.dart';
+import 'package:bts_lyrics_app/screens/members/yoongi.dart';
 import 'package:flutter/material.dart';
 
 const Color appUILightColor = Color.fromRGBO(180, 136, 212, 1);
@@ -18,3 +30,50 @@ String getBt21Pic() {
   bt21Pics.shuffle();
   return bt21Pics.first;
 }
+
+const Map<String, Widget> discography = {
+  'images/digital-singles.jpg': DigitalSingles(),
+  'images/kr-albums.jpg': AlbumsKR(),
+  'images/jp-albums.jpg': AlbumsJP(),
+  'images/uo-albums.jpg': AlbumsUO(),
+  'images/uo-songs2.jpg': SongsUO(),
+};
+
+const List<Map<String, dynamic>> soloProjects = [
+  {
+    'imageAsset': 'images/members/joon.jpg',
+    'name': 'RM',
+    'widget': Namjoon(),
+  },
+  {
+    'imageAsset': 'images/members/jin.jpg',
+    'name': 'Jin',
+    'widget': Jin(),
+  },
+  {
+    'imageAsset': 'images/members/yoongi.jpg',
+    'name': 'SUGA / Agust D',
+    'widget': Yoongi(),
+  },
+  {
+    'imageAsset': 'images/members/hobi.jpg',
+    'name': 'j-hope',
+    'widget': Hoseok(),
+  },
+  {
+    'imageAsset': 'images/members/jimin.jpg',
+    'name': 'Jimin',
+    'widget': Jimin(),
+  },
+  {
+    'imageAsset': 'images/members/tae.jpg',
+    'name': 'V',
+    'widget': Taehyung(),
+  },
+  {
+    'imageAsset': 'images/members/jk.jpg',
+    'name': 'Jungkook',
+    'widget': Jungkook(),
+  },
+
+];
