aa6f955066f769f2e2fc7b5b9da448a1e48a9aaf
KimVinod <79586514+KimVinod@users.noreply.github.com>
2026-02-04 19:34:46 +0530

    fix: Improve error logging

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -5,6 +5,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
@@ -27,10 +28,27 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 class FirebaseService {
   static void _setupCrashlytics(bool isRelease) {
     if(isRelease) {
-      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+      // Pass all uncaught "fatal" errors from the framework to Crashlytics
+      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+      // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+      PlatformDispatcher.instance.onError = (error, stack) {
+        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+        return true;
+      };
     } else {
       FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
       FirebaseCrashlytics.instance.deleteUnsentReports();
+    }
+  }
+
+  static void logCustomError(Exception e, StackTrace s, String? functionName) {
+    if(kReleaseMode) {
+      FirebaseCrashlytics.instance.recordError(
+        e,
+        s,
+        fatal: true,
+        reason: functionName != null ? 'Error in $functionName' : null,
+      );
     }
   }
 
